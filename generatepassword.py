f38e1ba2194fc127b4c83e9817ebc040e365bc4e
Kailc <1553207267@qq.com>
2018-02-24 17:55:33 +0800

    Update generatepassword.py
    添加加解密代码，待完善

--- generatepassword.py
+++ generatepassword.py
@@ -4,6 +4,9 @@ import random
 import string
 import os
 import json
+from Crypto.Cipher import AES
+import base64
+
 
 # whitespace = ' \t\n\r\v\f'
 # ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
@@ -46,40 +49,93 @@ def createpasswd(needlowercase = True, needuppercase = True, needdigits = True, 
 
     return passwd
 
-def createjson(sitename, username):
-
+def createdict(sitename, username, dict_text = None):
+    
     sitename = sitename.lower ().replace(' ', '')
     username = username.lower ().replace ( ' ' , '' )
-    path = os.getcwd() + '\\xxxx'
-    load_dict = {}
-    if os.name != 'nt':
-        path = path.replace('\\', '/')
-    if os.path.isfile(path):
-        with open ( path , 'r' ) as f:
-            load_dict = json.load ( f )
-            f.close ()
-            if (sitename + ' ' + username) in load_dict:
-                print ( 'Sitename:' , sitename , '\nUsername:' , username , '\nPasswd:' ,
-                        load_dict[ sitename + ' ' + username ] )
-                return
-
+    load_dict = dict_text
+    
+    # path = os.getcwd() + '\\xxxx'
+    # if os.name != 'nt':
+    #     path = path.replace('\\', '/')
+    # if os.path.isfile(path):
+    #     with open ( path , 'r' ) as f:
+    #         load_dict = json.load ( f )
+    #         f.close ()
+    #         if (sitename + ' ' + username) in load_dict:
+    #             print ( 'Sitename:', sitename, '\nUsername:', username, '\nPasswd:',
+    #                     load_dict[ sitename + ' ' + username ] )
+    #             return load_dict
+    if load_dict == dict and load_dict != None and (sitename + ' ' + username) in load_dict:
+        print ( 'Account already exists!\n', 'Sitename:' , sitename , '\nUsername:' , username , '\nPasswd:' ,
+                load_dict[ sitename + ' ' + username ] )
+        return load_dict
 
     passwd = createpasswd ()
     passwd_dict = {
         sitename + ' ' + username: passwd
     }
 
-    for i in load_dict:
-        passwd_dict[i] = load_dict[i]
+    if load_dict == dict and load_dict != None:
+        for i in load_dict:
+            passwd_dict[i] = load_dict[i]
+        
+    print ( 'Sitename:', sitename, '\nUsername:', username, '\nPasswd:', passwd )
+    return passwd_dict
 
-    with open (path, 'w') as f:
-        json.dump(passwd_dict, f, indent=4)
-        f.close()
-    print ( 'Sitename:' , sitename , '\nUsername:' , username , '\nPasswd:' , passwd )
+def tobytesandpad16(text):
+    # text = str.encode ( text )
+    while len(text) % 16 != 0:
+        text += '\0'
+    return str.encode ( text )
 
+def pad16(text):
+    while len(text) % 16 != 0:
+        text += b' '
+    return text
+
+'''
+mod = 0 encrypt
+mod = 1 decrypt
+'''
+def encryptanddecrypt_aes(text, mod = 0, secret_key = 'abcdefghijklmnop'):
+    if mod != 0 and mod != 1:
+        print('mode error', mod)
+        return None
+
+    aes = AES.new(tobytesandpad16(secret_key), AES.MODE_ECB)
+    if mod == 0:
+        return str(base64.encodebytes(aes.encrypt ( tobytesandpad16 ( text ) )), encoding='utf-8')
+    else:
+        return str(aes.decrypt(base64.decodebytes(text.encode(encoding='utf-8'))),encoding='utf-8')
+
+def getcipher(fileame = 'xxxx'):
+    text = None
+    path = os.getcwd() + '\\' + fileame
+    if os.name != 'nt':
+        path = path.replace('\\', '/')
+    if os.path.isfile(path):
+        with open ( path , 'r' ) as f:
+            text = f.read()
+            f.close()
+    return text
+
+def storecipher(text = '', fileame = 'xxxx'):
+    path = os.getcwd () + '\\' + fileame
+    if os.name != 'nt':
+        path = path.replace('\\', '/')
+    with open ( path , 'w' ) as f:
+        f.write ( text )
+        f.close ()
+    print('store in', path)
+        
 
 def main():
-    createjson ( 'sitename' , 'username' )
-
+    ciphertext = getcipher()
+    if ciphertext != None:
+        cleartext = encryptanddecrypt_aes(ciphertext, mod=1)
+    # print(type(eval(cleartext)))
+    createdict('aaa', 'ssss', eval(cleartext))
+    
 if __name__ == '__main__':
     main()
